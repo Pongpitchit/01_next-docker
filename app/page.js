42a9cbb0d7a82f837a80ba6987e425477013a207
Pongpitchit <Pongpitchitza@gmail.com>
2025-09-15 23:17:38 +0700

    update

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ import Attractions from './components/Attractions';
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-50">
-      <div>Page </div>
       <Attractions />
     </main>
   );
