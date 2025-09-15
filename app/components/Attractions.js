09d8f5bdcc92ef55ec6d0350eb9bfd656b3acd8c
Pongpitchit <Pongpitchitza@gmail.com>
2025-09-15 23:28:13 +0700

    add name

--- app/components/Attractions.js
+++ app/components/Attractions.js
@@ -50,6 +50,7 @@ export default function Attractions() {
       <h1 className="text-4xl font-bold text-center mb-10 text-gray-800">
         Popular Tourist Attractionss
       </h1>
+      <p className='text-3sm font-bold text-center mb-10 text-gray-800'>6607263 | Pongpitchit Sittipakdee</p>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {attractions.map((attraction) => (
