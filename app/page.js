a147154226e9ff35b4d844535a593fe46af61306
Besttpopxlarr <Pongpitchitza@gmail.com>
2025-09-15 22:59:39 +0700

    commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Attractions from './components/Attractions';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <div>Page </div>
+      <Attractions />
+    </main>
+  );
+}
